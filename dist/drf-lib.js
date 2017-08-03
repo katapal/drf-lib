6149b313d92f2a52201e8bc1f271c51cddae2ca5
David Xiao <dxiao@katapal.com>
2017-08-03 14:07:42 -0400

    handle error objects

--- dist/drf-lib.js
+++ dist/drf-lib.js
@@ -8,19 +8,23 @@ var errorParser = function(lowercaseFilter, ucfirstFilter) {
 errorParser.$inject = ['lowercaseFilter', 'ucfirstFilter'];
 
 errorParser.prototype.extractMessage = function(response) {
-  var self = this;
+  var self = this, i, extracted, msg, field;
   if (response.data && response.data.non_field_errors)
     return response.data.non_field_errors.join(' ');
   else if (response.data && response.data.detail)
     return response.data.detail;
-  else if (response.data && angular.isArray(response.data))
-    return response.data.join(', ');
-  else if (response.status == 400) {
+  else if (response.data && angular.isArray(response.data)) {
+    extracted = [];
+    for(i = 0; i < response.data.length; i++) {
+      extracted.push(self.extractMessage(response.data[i]));
+    }
+    return extracted.join(', ');
+  } else if (response.status == 400) {
     if (angular.isString(response.data))
       return response.data;
     else {
-      var msg = "";
-      for (var field in response.data) {
+      msg = "";
+      for (field in response.data) {
         msg += self.ucfirstFilter(self.lowercaseFilter(field)) +
           ": " + response.data[field] + " ";
       }
@@ -32,7 +36,14 @@ errorParser.prototype.extractMessage = function(response) {
     return response.message;
   else if (angular.isString(response))
     return response;
-  else
+  else if (angular.isObject(response)) {
+    msg = "";
+    for (field in response) {
+      msg += self.ucfirstFilter(self.lowercaseFilter(field)) +
+        ": " + response[field] + " ";
+    }
+    return msg;
+  } else
     return "Service unavailable";
 };
 
