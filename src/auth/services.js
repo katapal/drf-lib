40095d963d263f56fcd4f05c4de8a91d450f4f32
David Xiao <dxiao@katapal.com>
2017-09-08 10:57:03 -0400

    cap max refresh delay

--- src/auth/services.js
+++ src/auth/services.js
@@ -219,8 +219,12 @@ authService.prototype.setJWT = function(leeway, minDelay) {
     // recoverable
 
     if (!self.refreshPromise && (e.status < 0 || e.status >= 500)) {
-      self.refreshErrorDelay = self.refreshErrorDelay || (Math.random() + 1);
+      self.refreshErrorDelay = self.refreshErrorDelay ||
+        (Math.random() + 1)*1000;
       self.refreshErrorDelay *= 2;
+
+      // set max refresh delay to 15 minutes
+      self.refreshErrorDelay = Math.min(self.refreshErrorDelay, 15*60*1000);
 
       self.refreshPromise = self.$timeout(function () {
           delete self.refreshPromise;
